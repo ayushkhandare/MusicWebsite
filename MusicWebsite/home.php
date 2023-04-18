856b09548b7066e749679c0fe323b051c9231644
ayushkhandare <108357023+ayushkhandare@users.noreply.github.com>
2023-04-18 16:16:25 +0530

    Add files via upload

---
+++ MusicWebsite/home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Real Sound</title>
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+</head>
+<body>
+    <header>
+        <div class="navbar">
+            <a href="#" class="logo">
+                <i class="bi bi-music-note-list"></i>
+                The Real Sound
+            </a>
+            <a href="#" class="active">Home</a>
+            <a href="music.php">Music</a>
+            <a href="signup.php" class="signup">Create an account</a>
+            <a href="login.php" class="login">Login &ensp;|</a>
+        </div>
+        <div class="section">
+            <div class="tagline1">
+                Let's Listen
+            </div>
+            <div class="tagline2">
+                &ensp;Real Sound
+            </div>
+            <div class="sec_btn">
+                    <button type="button">GET PREMIUM</button>
+                <a href="signup.php">
+                    <button type="button" class="freetrail">START FREE TRAIL</button>
+                </a>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
